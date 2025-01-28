7de80177397a9a26fc6893d3cfe1b5ba8a5430f5
Mohcine <mohcine.aazzou@gmail.com>
2025-01-28 20:27:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="flex justify-center items-center min-h-screen bg-green-500">
+      <h1 className="text-white text-3xl font-bold">Bienvenue dans votre projet avec Tailwind CSS !</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
