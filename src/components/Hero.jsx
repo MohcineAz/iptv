82ab5e46046096d103ad6e3e7554e7260b3776a5
Mohcine <mohcine.aazzou@gmail.com>
2025-01-29 13:57:55 +0100

    Update bg color

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,10 @@ import Tool from './Tool'
 import hero from '../assets/images/hero.png'
 import RoundBtn from './RoundBtn'
 import yt from '../assets/images/youtube.png'
-import fb from '../assets/images/fb.png'
+
 function Hero() {
   return (
-    <section className='bg-[#00050E] overflow-hidden relative px-8 py-8'>
+    <section className='bg-[#00050E] bg-gradient-to-r from-[#00050E] from-10% via-[#8000FF] via-100% to-[#AD00FF] to-70% overflow-hidden relative px-8 py-8'>
       <Navbar />
       <div className="container grid grid-cols-1 md:grid-cols-2 min-h-[650px]">
         {/* Brand info */}
@@ -18,7 +18,10 @@ function Hero() {
                 <p className='text-white'>Welcome to Streamify – your gateway to limitless entertainment. With our cutting-edge IPTV service, enjoy live TV channels and on-demand content for an unparalleled streaming experience. Elevate your entertainment journey with Streamify today!</p>
                 <div className="group-btn flex items-center gap-4">
                     <Button text='Try now'/>
-                    <RoundBtn image={yt} />
+                    <button className='border-2 border-white rounded-full w-10 h-10 flex items-center justify-center'>
+                      <img src={yt} alt="" />
+                    </button>
+                    {/* <RoundBtn src={fb} /> */}
                 </div>
                 <div className="images">
                     <Tool />
