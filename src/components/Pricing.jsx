0e36827c3bc7b5298a196cc44ed2fd263ab38809
Mohcine <mohcine.aazzou@gmail.com>
2025-02-15 22:27:29 +0100

    Add Priceing Cards

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,10 +1,47 @@
 import React from "react";
-
+import PricingCard from "./PricingCard";
+const pricingData = [
+  {
+      id: 1,
+      month : '12 months',
+      price: '$59.99',
+      free : '(+1 month free)',
+      features : ['HD / FHD / 4K / 8K IPTV','+15 000 Live Channels','+20 000 Movies','+4 000 TV shows','2 simultaneous Connections']
+  },
+  {
+    id: 2,
+    month : '6 months',
+    price: '$29.99',
+    free : '(+1 month free)',
+    features : ['HD / FHD / 4K / 8K IPTV','+15 000 Live Channels','+20 000 Movies','+4 000 TV shows','2 simultaneous Connections']
+  },
+  {
+    id: 3,
+    month : '3 months',
+    price: '$14.99',
+    free : '(+1 month free)',
+    features : ['HD / FHD / 4K / 8K IPTV','+15 000 Live Channels','+20 000 Movies','+4 000 TV shows','2 simultaneous Connections']
+  },
+  {
+    id: 4,
+    month : '1 month',
+    price: '$9.99',
+    free : '(+1 month free)',
+    features : ['HD / FHD / 4K / 8K IPTV','+15 000 Live Channels','+20 000 Movies','+4 000 TV shows','2 simultaneous Connections']
+  }
+]
 function Pricing() {
   return (
     <section className="bg-[#00050E] py-12 px-8">
       <div className="title text-white text-center text-2xl">
         Choose your plan
+      </div>
+      <div className="py-12 grid gap-4 grid-cols-4 place-content-center place-items-center items-stretch">
+        {
+          pricingData.map((plan, index) =>
+            <PricingCard key={index} {...plan} />
+          )
+        }
       </div>
     </section>
   );
---
+++ src/components/PricingCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Button from './Button'
+
+function PricingCard({month, price, free, features}) {
+  return (
+    <section>
+      <div className='min-w-[300px] bg-[#0D1728] text-white rounded-3xl h-full text-center flex flex-col justify-between px-4 border-transparent hover:border-2 hover:border-amber-200 transition-all duration-300'>
+       <h3 className='text-sm py-3'>{month}</h3>
+       <p className='font-semibold text-2xl py-3'>{price}</p>
+       <h3 className='text-sm py-3'>{free}</h3>
+       <ul className='flex flex-col grow py-3 border-gray-400 border-b-2'>
+        {
+          features.map((feature, index) =>
+            <li key={index} className='flex-1 flex items-center justify-center py-3 border-gray-400 border-b-2 last:border-none'>{feature}</li>
+          )
+        }
+       </ul>
+       <div className='py-6 '>
+          <Button className='bg-white text-black' text='Subscribe'/>
+       </div>
+      
+      </div>
+    </section>
+  )
+}
+
+export default PricingCard
